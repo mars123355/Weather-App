101d1f57befc209df7f74fadad47e422d58cd04d
Arthur Pan <arthurpan24@gmail.com>
2015-10-04 16:04:49 -0700

    Changed comments

--- WeatherApp/WeatherApp/AppDelegate.h
+++ WeatherApp/WeatherApp/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  WeatherApp
 //
-//  Created by Marcel Osborne on 10/4/15.
+//  Created by Marcel Osborne, Arthur Pan, and JB Lanier on 10/4/15.
 //  Copyright © 2015 Marcel Osborne. All rights reserved.
 //
 
